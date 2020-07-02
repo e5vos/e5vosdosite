9f6e32aa032e34fa8b4de8f4bf85918ca516ef51
volubepossum <domj.roli@gmail.com>
2020-07-02 17:29:12 +0200

    fixing 404 on e5n home

--- app/Http/Controllers/E5N/PresentationController.php
+++ app/Http/Controllers/E5N/PresentationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\E5N;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Auth\Access\Response;
 use Illuminate\Support\Facades\Gate;

59a042802b7b54fd08f9f827a01f6be63a00ba3a
difcsi <mzolivagyok@gmail.com>
2020-07-09 16:42:45 +0200

    Added icons

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+require('laravel-mix-svg');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +12,17 @@ const mix = require('laravel-mix');
  |
  */
 
+
+
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+
+mix.js('resources/js/qrcreator.js','public/js/qr.js')
+
+mix.sass('resources/sass/app.scss', 'public/css')
+
+
+mix.copy('node_modules/bootstrap-icons/icons','public/svg/icons')
+
+
+
+mix.svg()

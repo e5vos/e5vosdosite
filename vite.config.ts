cbca61d6153d7606e0ec75ab4ac6f40465a7ffb4
difcsi <zoltan.m@tuta.io>
2022-08-27 22:25:46 +0100

    Add typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/ts/app.ts'],
+            input: 'resources/ts/app.tsx',
             refresh: true,
         }),
+        react(),
     ],
 });

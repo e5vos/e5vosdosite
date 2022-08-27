1babef977fd7a5546a2101d118f554c600709f6e
difcsi <zoltan.m@tuta.io>
2022-08-27 22:34:08 +0100

    Add breeze backend, tool configs

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+use App\Http\Controllers\{
+    Controller
+};
+
+use App\Models\{
+    User
+};
+
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Http\Request;
+
+
+use Illuminate\Support\Facades\{
+    Hash,
+    Auth,
+    URL,
+};
+
+
+class AuthController extends Controller{
+
+    public function redirect(Request $request, $provider='google')
+    {
+        $request->session()->put('intended_url', url()->previous());
+
+        return Socialite::driver($provider)->redirect();
+    }
+
+    public function callback(Request $request, $provider='google')
+    {
+        $userData = Socialite::driver($provider)->user();
+        $user = User::firstWhere('email',$userData->email);
+
+        if(!$user){
+            $user = User::create([
+                'name' => $userData->name,
+                'email' => $userData->email,
+                'google_id'=> Hash::make($userData->id)
+            ]);
+        }
+        if(!$user->google_id){
+            $user->google_id = Hash::make($userData->id);
+        }
+
+        if(!Hash::check($userData->id,$user->google_id)){
+            abort(400);
+        }
+
+        Auth::login($user);
+        return redirect()->to($request->session()->get('intended_url',''));
+    }
+
+    public function logout(Request $request){
+        Auth::logout();
+
+        return redirect()->route("index");
+    }
+
+    public function login(Request $request){
+        return $this->redirect($request,"google");
+    }
+
+
+
+}
--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -49,7 +49,7 @@ class LoginRequest extends FormRequest
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'email' => trans('auth.failed'),
+                'email' => __('auth.failed'),
             ]);
         }
 

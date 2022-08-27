874cc8049121fc77be7a3266d22e158d782964fb
difcsi <zoltan.m@tuta.io>
2022-08-27 22:11:45 +0100

    Add React Scaffolding

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: ['resources/css/app.css', 'resources/ts/app.ts'],
             refresh: true,
         }),
-        react(),
     ],
 });

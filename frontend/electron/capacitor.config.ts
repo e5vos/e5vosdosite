3778af41586e0a13ab815be25a45be587444b19a
difcsi <zoltan.m@tuta.io>
2022-09-18 02:49:37 +0100

    Add electron

---
+++ frontend/electron/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "hu.e5vosdo.app",
+  appName: "E5vös DÖ",
+  webDir: "dist",
+  bundledWebRuntime: false,
+};
+
+export default config;

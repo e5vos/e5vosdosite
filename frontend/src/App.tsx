9b7c0a1867819cd8dbd2ff7722f0314509b08bfb
difcsi <zoltan.m@tuta.io>
2022-09-18 02:07:34 +0100

    Add sass

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,10 +8,10 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" rel="noopener" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org"  rel="noopener" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>

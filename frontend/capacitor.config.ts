b3e9a892cdc7ac6b8da8beb93166a73391fc0ea9
difcsi <zoltan.m@tuta.io>
2022-09-28 21:08:08 +0100

    Add icon files

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -8,7 +8,6 @@ const config: CapacitorConfig = {
   appName: "E5vös DÖ",
   webDir: "dist",
   bundledWebRuntime: false,
-
   server:{
     url: "http://192.168.56.1:5173",
     cleartext: true,
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+
 const Home = () => {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };

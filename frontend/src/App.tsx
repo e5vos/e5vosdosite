4f7a841cbd3252d1858eb6c20f55ad2ef3aacc78
difcsi <zoltan.m@tuta.io>
2022-10-08 23:02:13 +0100

    Rescue App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
-import "style/App.scss";
+import "style/App.pcss";
 import { Provider } from "react-redux";
 import store from "lib/store";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { BaseLayout } from "templates";
 import React from "react";
 import Loader from "components/UIKit/Loader";
-import EventsPage from "pages/event";
 
 
 const Home = React.lazy(()=>import("pages/Home"))
 const Error = React.lazy(()=>import("components/Error"))
+const PresentationPage = React.lazy(() => import("pages/presentation"));
+const TeamsPage = React.lazy(() => import("pages/team"));
+const TeamPage = React.lazy(() => import("pages/team/[teamcode]"));
+const EventsPage = React.lazy(() => import("pages/event"));
+const EventPage = React.lazy(() => import("pages/event/[eventid]"));
 
 function App() {
   
@@ -19,9 +23,48 @@ function App() {
       <BrowserRouter>
         <BaseLayout>
           <React.Suspense fallback={<Loader/>}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="*" element={<Error code={404}  />} />
+          <Routes>
+              <Route path="/">
+                <Route index element={<Home />} />
+                <Route path="csapat">
+                  <Route index element={<TeamsPage />} />
+                  <Route path=":teamid">
+                    <Route index element={<TeamPage />} />
+                    <Route path="admin" element={<>TeamAdminPage</>} />
+                  </Route>
+                  <Route path="uj" element={<>NewTeamPage</>} />
+                  <Route path="admin" element={<>TeamAdminsPage</>} />
+                </Route>
+                <Route path="esemeny">
+                  <Route index element={<EventsPage />} />
+                  <Route path=":eventid">
+                    <Route index element={<EventPage />} />
+                    <Route path="kezel">
+                      <Route index element={<>ManageEventPage</>} />
+                      <Route path="szerkeszt" element={<>EditEventPage</>} />
+                      <Route path="scanner" element={<>ScannerPage</>} />
+                      <Route path="admin" element={<>ManageEventAdminPage</>} />
+                    </Route>
+                  </Route>
+                  <Route path="kezel">
+                    <Route index element={<>ManageEventsPage</>} />
+                    <Route path="admin" element={<>EventAdminsPage</>} />
+                  </Route>
+                </Route>
+                <Route path="eloadas">
+                  <Route index element={<PresentationPage/>} />
+                  <Route
+                    path=":presentationCode"
+                    element={<>PresentationAttendancePage</>}
+                  />
+                  <Route path="kezel">
+                    <Route index element={<>PresentationAttendancePage</>} />
+                    <Route path="admin" element={<>TeamAdminPage</>} />
+                  </Route>
+                </Route>
+
+                <Route path="*" element={<Error code={404} />} />
+              </Route>
             </Routes>
           </React.Suspense>
         </BaseLayout>

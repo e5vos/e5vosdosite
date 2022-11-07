1ffd6b67401f6b540b959e205ec04620596afbc4
difcsi <zoltan.m@tuta.io>
2022-11-07 01:04:48 +0100

    Fix footer css

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,22 @@
+import Button from "components/UIKit/Button";
+import { Link } from "react-router-dom";
+import { ReactComponent as Donci } from "assets/donci.svg";
 const Home = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="mx-4 max-h-screen">
+      <div className="container mx-auto">
+        <div className="mx-auto max-w-fit text-center my-5">
+          <h1 className="mb-3 font-bold text-6xl">Eötvös DÖ</h1>
+          <Button variant="primary">
+            <Link to="/eloadas">Előadásjelentkezés</Link>
+          </Button>
+          <div>
+            <Donci fill="white" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;

c6966291c25a462741c5136eab45780db8209b01
difcsi <zoltan.m@tuta.io>
2022-11-07 02:06:24 +0100

    Fix mobile navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -3,11 +3,7 @@ import Navbar from "components/UIKit/Navbar";
 const CustomNavbar = () => {
   return (
     <Navbar brand={<Navbar.Brand>Főoldal</Navbar.Brand>}>
-      <Navbar.Link href="">Test</Navbar.Link>
-      <Navbar.Dropdown toggle={<></>}>
-        <Navbar.Dropdown.Item>Test</Navbar.Dropdown.Item>
-      </Navbar.Dropdown>
-      <Navbar.Link href="">Test</Navbar.Link>
+      <Navbar.Link href="/eloadas">Előadásjelentkezés</Navbar.Link>
     </Navbar>
   );
 };
--- frontend/src/components/UIKit/Navbar/index.tsx
+++ frontend/src/components/UIKit/Navbar/index.tsx
@@ -9,7 +9,7 @@ const Brand = ({ href, children }: { href?: string; children: ReactNode }) => {
   return (
     <Tag href={href} className="flex items-center">
       <Donci fill="white" className="h-full w-12 mr-5" />
-      <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+      <span className="self-center text-xl font-semibold whitespace-nowrap">
         {children}
       </span>
     </Tag>
@@ -27,10 +27,7 @@ const DropDownItem = ({
   return (
     <Menu.Item>
       {({ active }) => (
-        <Tag
-          href={href}
-          className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-        >
+        <Tag href={href} className="block py-2 px-4 hover:bg-gray-100">
           {children}
         </Tag>
       )}
@@ -49,7 +46,7 @@ const DropDownComponent = ({
     <Menu as="li">
       {({ open }) => (
         <>
-          <Menu.Button className="flex justify-between items-center py-2 pr-4 pl-3 w-full font-medium text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-gray-400 dark:hover:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
+          <Menu.Button className="flex justify-between items-center py-2 pr-4 pl-3 w-full font-medium text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto">
             {toggle}
             <Caret />
           </Menu.Button>
@@ -65,13 +62,13 @@ const DropDownComponent = ({
           >
             <Menu.Items
               static
-              className="z-10 w-44 font-normal bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
+              className="z-10 w-44 font-normal bg-white rounded divide-y divide-gray-100 shadow"
               style={{
                 visibility: open ? "visible" : "hidden",
               }}
             >
               <ul
-                className="py-1 text-sm text-gray-700 dark:text-gray-400"
+                className="py-1 text-sm text-gray-700"
                 aria-labelledby="dropdownLargeButton"
               >
                 {children}
@@ -89,7 +86,7 @@ const Link = ({ href, children }: { href: string; children: ReactNode }) => {
     <li>
       <a
         href={href}
-        className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+        className="block py-2 pr-4 pl-3 text-white rounded hover:bg-gray-100 md:hover:bg-inherit md:hover:text-gray-50 md:p-0"
       >
         {children}
       </a>
@@ -106,46 +103,46 @@ const Navbar = ({
 }) => {
   const isMobile = useIsMobile();
   return (
-    <nav className="px-2 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-      <Menu
-        as="div"
-        className="container flex flex-wrap justify-between items-center mx-auto"
-      >
+    <nav className="px-2 py-2 mb-2 bg-gray-400 border-gray-200 rounded text-white">
+      <Menu as="div" className="">
         {({ open }) => (
-          <>
-            <>{brand}</>
-            <Menu.Button
-              className="inline-flex justify-center items-center ml-3 text-gray-400 rounded-lg md:hidden hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-500"
-              aria-controls="mobile-menu-2"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Főmenü megnyitása</span>
-              <MobileMenu />
-            </Menu.Button>
+          <div>
+            <div className="container flex flex-wrap justify-between items-center mx-auto">
+              <>{brand}</>
+              <Menu.Button
+                className="inline-flex justify-center items-center ml-3 md:hidden  rounded-lg hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300"
+                aria-controls="mobile-menu-2"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Főmenü megnyitása</span>
+                <MobileMenu fill="white" className="h-10" />
+              </Menu.Button>
+              <ul className="hidden md:flex p-4 flex-row space-x-8 mt-0 text-sm font-medium rounded-lg">
+                {children}
+              </ul>
+            </div>
             <Transition
               show={!isMobile || open}
-              enter="transition duration-100 ease-out"
+              enter="transition duration-200 ease-out"
               enterFrom="transform scale-95 opacity-0"
               enterTo="transform scale-100 opacity-100"
-              leave="transition duration-75 ease-out"
+              leave="transition duration-200 ease-out"
               leaveFrom="transform scale-100 opacity-100"
               leaveTo="transform scale-95 opacity-0"
               static
             >
               <Menu.Items
-                className="w-full md:block md:w-auto"
+                className={`w-full ${isMobile && open ? "visible" : "hidden"}`}
                 id="mobile-menu"
                 static
-                style={{
-                  visibility: !isMobile || open ? "visible" : "hidden",
-                }}
+                as="div"
               >
-                <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <ul className="flex flex-col p-4 mt-4 rounded-lg border font-medium">
                   {children}
                 </ul>
               </Menu.Items>
             </Transition>
-          </>
+          </div>
         )}
       </Menu>
     </nav>

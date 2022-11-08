1febc624fac20cb55cdfdec31372d2f7db726aaf
difcsi <zoltan.m@tuta.io>
2022-11-08 18:14:06 +0000

    Add frontend to 404

--- backend/resources/views/errors/404.blade.php
+++ backend/resources/views/errors/404.blade.php
@@ -1,5 +1,27 @@
-@extends('errors::minimal')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" type="image/svg+xml" href="/whitedonci.svg" />
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+    <link rel="mask-icon" href="/mask-icon.svg" color="#FFFFFF" />
+
+    <meta name="msapplication-TileColor" content="#FFFFFF" />
+    <meta name="theme-color" content="#FFFFFF" />
+
+    <title>Eötvös DÖ</title>
+    <meta name="description" content="Vite + React + TS" />
+    <script type="module" crossorigin src="/assets/index.632661cb.js"></script>
+    <link rel="stylesheet" href="/assets/index.35fb000d.css">
+    <link rel="manifest" href="/manifest.webmanifest">
+    <script id="vite-plugin-pwa:register-sw" src="/registerSW.js"></script>
+</head>
+
+<body>
+    <div id="root"></div>
+
+</body>
+
+</html>

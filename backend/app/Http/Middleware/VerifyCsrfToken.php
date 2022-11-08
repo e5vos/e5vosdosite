5df75f34f0a9fdff544a0e006659a7974a8924ee
volubepossum <domj.roli@gmail.com>
2022-11-08 17:10:33 +0100

    turn csrf on

--- backend/app/Http/Middleware/VerifyCsrfToken.php
+++ backend/app/Http/Middleware/VerifyCsrfToken.php
@@ -11,5 +11,7 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array<int, string>
      */
-    protected $except = ["*"];
+    protected $except = [
+        //
+    ];
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -131,7 +131,6 @@ Route::prefix('/setting')->middleware(['auth:sanctum'])->controller(SettingContr
 );
 
 //test websocket broadcast
-
 Route::post('/ping', function () {
     Ping::dispatch();
 });

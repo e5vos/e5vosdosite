87012e6a6cee1a943cf8ae94fde0a8b33ca593ac
difcsi <zoltan.m@tuta.io>
2023-10-30 13:29:57 +0000

    Add php optimalisations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }

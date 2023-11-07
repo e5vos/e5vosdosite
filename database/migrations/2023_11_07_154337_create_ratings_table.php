3c4eac4037ef280b1ba8d1e9bdc71ac62913f53e
difcsi <zoltan.m@tuta.io>
2023-11-07 14:47:07 +0000

    add missing Rating model

---
+++ database/migrations/2023_11_07_154337_create_ratings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Event;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Event::class);
+            $table->smallInteger('rating', false, true)->comment('out of ten');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+};

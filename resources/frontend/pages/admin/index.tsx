45352e12b13b28ffeed50bdedc9df11ca30b5b59
Barnabás Gőz <barnagoz@icloud.com>
2024-02-29 23:53:56 +0100

    feat: remove hard coded program names

--- resources/frontend/pages/admin/index.tsx
+++ resources/frontend/pages/admin/index.tsx
@@ -10,70 +10,68 @@ import Button from "components/UIKit/Button";
 import Dialog from "components/UIKit/Dialog";
 
 const locale = Locale({
-    hu: {
-        title: "Eötvös Napok Adminisztrátor",
-        system: "Rendszer",
-        cacheClear: "Cache törlése",
-        UsersDialog: {
-            title: "Felhasználók",
-            description: "Az összes felhasználó listája",
-        },
-        eventsDialog: {
-            title: "Események",
-        },
-        slots: {
-            title: "Sávok",
-            button: "Sávok kezelése",
-        },
+  hu: {
+    title: `${import.meta.env.VITE_EVENT_HU} Adminisztrátor`,
+    system: "Rendszer",
+    cacheClear: "Cache törlése",
+    UsersDialog: {
+      title: "Felhasználók",
+      description: "Az összes felhasználó listája",
     },
-    en: {
-        title: "Eötvös Napok Admin",
-        system: "System",
-        cacheClear: "Clear cache",
-        UsersDialog: {
-            title: "Users",
-            description: "List of all users",
-        },
-        eventsDialog: {
-            title: "Events",
-        },
-        slots: {
-            title: "Slots",
-            button: "Manage slots",
-        },
+    eventsDialog: {
+      title: "Események",
     },
+    slots: {
+      title: "Sávok",
+      button: "Sávok kezelése",
+    },
+  },
+  en: {
+    title: `${import.meta.env.VITE_EVENT_EN} Admin`,
+    system: "System",
+    cacheClear: "Clear cache",
+    UsersDialog: {
+      title: "Users",
+      description: "List of all users",
+    },
+    eventsDialog: {
+      title: "Events",
+    },
+    slots: {
+      title: "Slots",
+      button: "Manage slots",
+    },
+  },
 });
 
 const AdminPage = () => {
-    const [clearCache] = adminAPI.useClearCacheMutation();
-    const navigate = useNavigate();
-    const [usersDialogOpen, setUsersDialogOpen] = useState(false);
-    const [eventsDialogOpen, setEventsDialogOpen] = useState(false);
-    return (
-        <>
-            <div className="mx-auto max-w-4xl">
-                <h1 className="text-center text-4xl font-bold">
-                    {locale.title}
-                </h1>
-                <div className="mt-4 flex gap-64">
-                    <div className="">
-                        <h3 className="mb-3 text-center text-2xl font-bold">
-                            {locale.system}
-                        </h3>
-                        <Button
-                            onClick={async () => {
-                                await clearCache();
-                            }}
-                            variant="danger"
-                            className="p-3"
-                        >
-                            {locale.cacheClear}
-                        </Button>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  const [clearCache] = adminAPI.useClearCacheMutation();
+  const navigate = useNavigate();
+  const [usersDialogOpen, setUsersDialogOpen] = useState(false);
+  const [eventsDialogOpen, setEventsDialogOpen] = useState(false);
+  return (
+    <>
+      <div className="mx-auto max-w-4xl">
+        <h1 className="text-center text-4xl font-bold">{locale.title}</h1>
+        <div className="mt-4 flex gap-64">
+          <div className="">
+            <h3 className="mb-3 text-center text-2xl font-bold">
+              {locale.system}
+            </h3>
+            <Button
+              onClick={async () => {
+                await clearCache();
+              }}
+              variant="danger"
+              className="p-3"
+            >
+              {locale.cacheClear}
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default gated(AdminPage, isAdmin);

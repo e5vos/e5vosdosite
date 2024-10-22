c792f990e60727eba62ef42e6da1b1ebb1c3f62f
difcsi <zoltan.m@tuta.io>
2024-10-22 11:40:13 +0100

    fix: ver

--- ci/deployver.sh
+++ ci/deployver.sh
@@ -1,4 +1,4 @@
 FRONTEND_VERSION=$(node -p -e "require('./package.json').version")
 BACKEND_VERSION=$(node -p -e "require('./composer.json').version")
-DATE_VERSION=$(date +%Y.%m.%d)
+DATE_VERSION=$(date +%Y.%m.%d%H.%M)
 echo $DATE_VERSION-$1
